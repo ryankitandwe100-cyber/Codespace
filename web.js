39af2874a29ea22ebc092f2fadb94638d9c77bc3
ryankitandwe100-cyber <ryankitandwe100@gmail.com>
2026-01-05 17:58:39 +0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('button'); 
+const colors = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+function change() {
+   document.body.style.background = colors[Math.floor(7*Math.random())];
+}   
+button.addEventListener('click', change);
